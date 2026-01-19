d48b4aecaf796d7b632a874dadfc6e97158d0599
felipe <felipeferreira.rodrigo@gmail.com>
2026-01-18 21:26:47 -0300

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Portfolio from "./pages/Porfolio";
-
+import { Analytics } from "@vercel/analytics/next";
 function App() {
   return (
     <>
+      <Analytics />
       <Portfolio />
     </>
   );
